06571d4a51b9309c86e187e798a429a08ce171e4
Andrey <makarav84@gmail.com>
2020-02-18 21:11:34 +0200

    first commit

---
+++ Lesson-1/script.js
@@ -0,0 +1,69 @@
+'use strict';
+
+// a = 5;
+// console.log(a);
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+console.log(leftBorderWidth);
+
+
+var number = 5;
+var string = "HEllo!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string' * 9);
+
+let something;
+console.log(something);
+
+let persone = {
+    name: 'John',
+    age: 25,
+    isMaried: false
+};
+
+
+console.log(persone.name);
+console.log(persone['name']);
+
+let arr = ['plum.png' , 'orange.jpg' , 'apple.bmp'];
+
+console.log(arr[2]);
+
+// alert('Hello World!');
+
+// confirm('Are you sure?');
+
+// let answer = confirm('Are you sure?');
+// console.log(answer);
+
+// let answer = +prompt('Есть ли вам 18?', "Да");
+// console.log(typeof(answer));
+
+// console.log('arr' + '-object');
+// console.log(4 + '-object');
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+console.log('2' === 2);
+
+let isChecket = true,
+    isClose = false;
+
+console.log(isChecket && !isClose);
+console.log(isChecket || isClose);
